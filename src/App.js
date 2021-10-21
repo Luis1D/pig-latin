f92765466f6ca95af7069579c7c494ff7a1dc816
Luis1D <deanda_luis@outlook.com>
2021-10-21 16:51:50 -0700

    shortend the translate function & added an animation for submit

--- src/App.js
+++ src/App.js
@@ -4,37 +4,31 @@ import React, { useState } from 'react';
 function App() {
   const [pigLatin, setPigLatin] = useState({text: "", submited: false});
   const translate = (str) => {
-    let cleanupStr = str.trim();
-    if (/\s/.test(cleanupStr)) {
-      if (cleanupStr.split(" ").length > 0) {
-        let sentence = cleanupStr.split(" ");
-        let pig = sentence.map(word => {
-          let reg = /[aeiou]|[AEIOU]/
-          let matchStr = word.match(reg);
-          if (matchStr === null) {
-            return word += "ay"
-          }
-          if (matchStr.index > 0) {
-            let consonent = word.substr(0, matchStr.index);
-            let subStr = word.substr(matchStr.index);
-            let pigLatin = subStr += consonent += "ay";
-            return pigLatin;
-          }
-          return word += "way";
-        })
-        return pig.join(" ").toLowerCase();
-      }
-    }
+    let cleanupStr = str.trim().toLowerCase();
     let reg = /[aeiou]|[AEIOU]/
+    if (/\s/.test(cleanupStr)) {
+      let sentence = cleanupStr.split(" ").map(word => {
+        let matchStr = word.match(reg);
+        if (matchStr === null) {
+          return word += "ay"
+        }
+        if (matchStr.index > 0) {
+          let consonent = word.substr(0, matchStr.index);
+          let subStr = word.substr(matchStr.index);
+          return subStr += consonent += "ay";
+        }
+        return word += "way";
+      })
+      return sentence.join(" ");
+    }
     let matchStr = cleanupStr.match(reg);
     if (matchStr === null) {
-      return cleanupStr += "ay"
+      return cleanupStr += "ay";
     }
     if (matchStr.index > 0) {
       let consonent = cleanupStr.substr(0, matchStr.index);
       let subStr = cleanupStr.substr(matchStr.index);
-      let pigLatin = subStr += consonent += "ay";
-      return pigLatin;
+      return subStr += consonent += "ay";
     }
     return cleanupStr += "way";
   }
@@ -52,7 +46,11 @@ function App() {
   return (
     <div className="App">
       <div className="header">
-        <img src="pig.png" alt="Pigy"/>
+        <img 
+          className={ pigLatin.submited ? "triggerPig" : null }
+          src="pig.png" 
+          alt="Pigy"
+        />
         <h1>Pig Latin Translator</h1>
       </div>
       <form onSubmit={ handleSubmit }>
